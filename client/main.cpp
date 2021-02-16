43457354c8a43276c1fcda793988a8abc519cf55
Mykyta Savytskyi <nik965@bigmir.net>
2021-02-16 19:19:33 +0200

    moved .  view  problem

--- client/main.cpp
+++ client/main.cpp
@@ -1,10 +1,15 @@
 #include "client.h"
 
 #include <QCoreApplication>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include "usermanager.h"
 
 int main(int argc, char *argv[]) {
-    QCoreApplication a(argc, argv);
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
+    QGuiApplication app(argc, argv);
+    UserManager manager(nullptr);
     if (argc != 3) {
         qDebug() << "usage ./uTracker [ip] [port]";
         return 1;
@@ -13,5 +18,5 @@ int main(int argc, char *argv[]) {
     client.doConnect(argv[1], QString(argv[2]).toInt());
     client.testRequestLoop();
 
-    return a.exec();
+    return app.exec();
 }
